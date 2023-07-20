e75bbd5864ee76af4aedccb9472b828d6ab2fbc4
Vikash Basant <basantvikash360@gmail.com>
2023-07-20 17:43:30 +0530

    EnableAutoConfiguration, ComponentScan, and SpringBootConfiguration annotation are building block of spring boot application

--- src/main/java/HelloSpringBootApplication.java
+++ src/main/java/HelloSpringBootApplication.java
@@ -1,13 +1,28 @@
+import com.kash.controller.College;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
+@SpringBootConfiguration
 @ComponentScan("com.kash.controller")
 @EnableAutoConfiguration
 public class HelloSpringBootApplication {
     public static void main(String[] args) {
         // start app from here:
         SpringApplication springApplication = new SpringApplication(HelloSpringBootApplication.class);
-        springApplication.run();
+        ConfigurableApplicationContext container = springApplication.run();
+
+        College college = container.getBean(College.class); // college object:
+        System.out.println("-------------------");
+        System.out.println("college object: " + college);
+        college.printCollegeName();
+    }
+
+    @Bean
+    public College college() {
+        return new College();
     }
 }
---
+++ src/main/java/com/kash/controller/College.java
@@ -0,0 +1,10 @@
+package com.kash.controller;
+
+import org.springframework.stereotype.Component;
+
+
+public class College {
+    public void printCollegeName() {
+        System.out.println("college name is: IIT");
+    }
+}
